41a35ec2fd4c48e6c4e1adc598056f7b07dc43fa
mutsuyuki <mutsuyuki.tanaka@gmail.com>
2023-04-20 11:46:35 +0900

    change : 丸描画の塗りつぶしなしモード追加

--- src/components/Canvas/Renderer/Circle.ts
+++ src/components/Canvas/Renderer/Circle.ts
@@ -10,18 +10,19 @@ export default class Circle implements Graphic {
   private _center: Point;
   private _radius: number;
   private _color: Color;
+  private _filled: boolean;
 
-  constructor(center: Point, radius: number, color: Color) {
+  constructor(center: Point, radius: number, color: Color, filled: boolean = true) {
     this._center = DeepCloner.copy(center);
     this._radius = radius;
     this._color = DeepCloner.copy(color);
+    this._filled = filled;
   }
 
   public draw(context: CanvasRenderingContext2D) {
     const scale = {x: context.canvas.clientWidth, y: context.canvas.clientHeight};
 
     context.beginPath();
-    context.fillStyle = ColorUtil.rgba(this._color);
     context.arc(
       this._center.x * scale.x,
       this._center.y * scale.y,
@@ -30,7 +31,14 @@ export default class Circle implements Graphic {
       Math.PI * 2,      // end angle
       false
     );
-    context.fill();
+    if (this._filled) {
+      context.fillStyle = ColorUtil.rgba(this._color);
+      context.fill();
+    } else {
+      context.strokeStyle = ColorUtil.rgba(this._color);
+      context.lineWidth = 1;
+      context.stroke();
+    }
     context.closePath();
   };
 
